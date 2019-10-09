9178c1b5b5b7259cbade6aee72142f0c14ce0271
chenzhiwei <chenzhiwei@diligrp.com>
2019-10-09 15:30:00 +0800

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.mail.MailSendUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println(MailSendUtil.getFormName()+":"+MailSendUtil.getPassword());
 	}
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+
 }
